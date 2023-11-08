72707f20509ecb62a208af38d927bba338264193
varathan4545 <varatharajan@techorin.lk>
2023-11-09 00:14:47 +0530

    merge main

--- src/components/project/Project.tsx
+++ src/components/project/Project.tsx
@@ -1,83 +1,112 @@
+import { useEffect } from "react"
 
 
 type Props = {}
 
-export default function Project({}: Props) {
+export default function Project({}: Props) { 
+	
+	useEffect(() => {
+		// @ts-ignore
+		const $carousel = $('.project-carousel');
+		if ($carousel.length) {
+			 // Check selected elements
+			$carousel.owlCarousel({
+			  rtl: true,
+			  loop: true,
+			  margin: 20,
+			  nav: false,
+			  smartSpeed: 500,
+			  autoHeight: true,
+			  autoplay: true,
+			  autoplayTimeout: 10000,
+			  navText: ['<span class="fa fa-long-arrow-alt-left"></span>', '<span class="fa fa-long-arrow-alt-right"></span>'],
+			  responsive: {
+				0: {
+				  items: 1
+				},
+				768: {
+				  items: 2
+				},
+				1024: {
+				  items: 3
+				},
+				1200: {
+				  items: 4
+				},
+			  }
+			});
+		  }
+	
+	}, [])
+	
   return (
     <>
-   {/* <!-- Project Section --> */}
+  {/* <!-- Project Section --> */}
 	<section className="project-section pull-up pb-0">
 		<div className="large-container">
 			<div className="sec-title text-center">
 				<span className="sub-title">LASTEST PROJECT</span>
 				<h2>Our Latest Projects</h2>
 			</div>
-			<div className="row wow fadeInUp">
-				<div className="col-xl-3 col-sm-6">
-					{/* <!-- Project Block --> */}
-					<div className="project-block mb-30">
-						<div className="inner-box">
-							<div className="image-box">
-								<figure className="image">
-									<a href="images/resource/project-1.jpg" className="lightbox-image"><img src="images/resource/project-1.jpg" alt="" /></a>
-								</figure>
-								<a href="page-project-details.html" className="icon"><i className="fa fa-plus"></i></a>
-							</div>
-							<div className="content-box">
-								<span className="sub-title">Solar Energy</span>
-								<h4 className="title"><a href="page-project-details.html">Maximizing Solar ROI</a></h4>
-							</div>
+			{/* <!-- Prject Carousel --> */}
+			<div className="project-carousel owl-carousel owl-theme wow fadeInUp">
+				{/* <!-- Project Block --> */}
+				<div className="project-block">
+					<div className="inner-box">
+						<div className="image-box">
+							<figure className="image">
+								<a href="images/resource/project-1.jpg" className="lightbox-image"><img src="images/resource/project-1.jpg" alt="" /></a>
+							</figure>
+							<a href="page-project-details.html" className="icon"><i className="fa fa-plus"></i></a>
+						</div>
+						<div className="content-box">
+							<span className="sub-title">Solar Energy</span>
+							<h4 className="title"><a href="page-project-details.html">Maximizing Solar ROI</a></h4>
 						</div>
 					</div>
 				</div>
-				<div className="col-xl-3 col-sm-6">
-					{/* <!-- Project Block --> */}
-					<div className="project-block mb-30">
-						<div className="inner-box">
-							<div className="image-box">
-								<figure className="image">
-									<a href="images/resource/project-2.jpg" className="lightbox-image"><img src="images/resource/project-2.jpg" alt="" /></a>
-								</figure>
-								<a href="page-project-details.html" className="icon"><i className="fa fa-plus"></i></a>
-							</div>
-							<div className="content-box">
-								<span className="sub-title">Solar Energy</span>
-								<h4 className="title"><a href="page-project-details.html">Diversifying Your Solar</a></h4>
-							</div>
+				{/* <!-- Project Block --> */}
+				<div className="project-block">
+					<div className="inner-box">
+						<div className="image-box">
+							<figure className="image">
+								<a href="images/resource/project-2.jpg" className="lightbox-image"><img src="images/resource/project-2.jpg" alt="" /></a>
+							</figure>
+							<a href="page-project-details.html" className="icon"><i className="fa fa-plus"></i></a>
+						</div>
+						<div className="content-box">
+							<span className="sub-title">Solar Energy</span>
+							<h4 className="title"><a href="page-project-details.html">Diversifying Your Solar</a></h4>
 						</div>
 					</div>
 				</div>
-				<div className="col-xl-3 col-sm-6">
-					{/* <!-- Project Block --> */}
-					<div className="project-block mb-30">
-						<div className="inner-box">
-							<div className="image-box">
-								<figure className="image">
-									<a href="images/resource/project-3.jpg" className="lightbox-image"><img src="images/resource/project-3.jpg" alt="" /></a>
-								</figure>
-								<a href="page-project-details.html" className="icon"><i className="fa fa-plus"></i></a>
-							</div>
-							<div className="content-box">
-								<span className="sub-title">Solar Energy</span>
-								<h4 className="title"><a href="page-project-details.html">The Benefits of Solar</a></h4>
-							</div>
+				{/* <!-- Project Block --> */}
+				<div className="project-block">
+					<div className="inner-box">
+						<div className="image-box">
+							<figure className="image">
+								<a href="images/resource/project-3.jpg" className="lightbox-image"><img src="images/resource/project-3.jpg" alt="" /></a>
+							</figure>
+							<a href="page-project-details.html" className="icon"><i className="fa fa-plus"></i></a>
+						</div>
+						<div className="content-box">
+							<span className="sub-title">Solar Energy</span>
+							<h4 className="title"><a href="page-project-details.html">The Benefits of Solar</a></h4>
 						</div>
 					</div>
 				</div>
-				<div className="col-xl-3 col-sm-6">
-					{/* <!-- Project Block --> */}
-					<div className="project-block mb-30">
-						<div className="inner-box">
-							<div className="image-box">
-								<figure className="image">
-									<a href="images/resource/project-4.jpg" className="lightbox-image"><img src="images/resource/project-4.jpg" alt="" /></a>
-								</figure>
-								<a href="page-project-details.html" className="icon"><i className="fa fa-plus"></i></a>
-							</div>
-							<div className="content-box">
-								<span className="sub-title">Solar Energy</span>
-								<h4 className="title"><a href="page-project-details.html">Shining a Light</a></h4>
-							</div>
+				{/* <!-- Project Block --> */}
+				<div className="project-block">
+					<div className="inner-box">
+						<div className="image-box">
+							<figure className="image">
+								<a href="images/resource/project-4.jpg" className="lightbox-image"><img src="images/resource/project-4.jpg" alt="" /></a>
+							</figure>
+							<a href="page-project-details.html" className="icon"><i className="fa fa-plus"></i></a>
+						</div>
+						<div className="content-box">
+							<span className="sub-title">Solar Energy</span>
+							<h4 className="title"><a href="page-project-details.html">Shining a Light</a></h4>
 						</div>
 					</div>
 				</div>
@@ -85,6 +114,7 @@ export default function Project({}: Props) {
 		</div>
 	</section>
 	{/* <!--End Project Section --> */}
+
     </>
   )
 }
--- src/components/team/Team.tsx
+++ src/components/team/Team.tsx
@@ -1,102 +1,165 @@
+import { useEffect } from "react";
 
 
 type Props = {}
 
 export default function Team({ }: Props) {
+    useEffect(() => {
+		// @ts-ignore
+		const $carousel = $('.four-items-carousel');
+		if ($carousel.length) {
+			 // Check selected elements
+			$carousel.owlCarousel({
+			  rtl: true,
+			  loop: true,
+			  margin: 20,
+			  nav: false,
+			  smartSpeed: 500,
+			  autoHeight: true,
+			  autoplay: true,
+			  autoplayTimeout: 10000,
+			  navText: ['<span class="fa fa-long-arrow-alt-left"></span>', '<span class="fa fa-long-arrow-alt-right"></span>'],
+			  responsive: {
+				0: {
+				  items: 1
+				},
+				768: {
+				  items: 2
+				},
+				1024: {
+				  items: 3
+				},
+				1200: {
+				  items: 4
+				},
+			  }
+			});
+		  }
+	
+	}, [])
     return (
         <>
             {/* <!-- Team Section Seven --> */}
-            <section className="team-section before_none pb-xs--0 pt-0">
-                <div className="auto-container">
-                    <div className="sec-title text-center">
-                        <span className="sub-title">MEET PROFESSIONALS</span>
-                        <h2>Our Expert Members</h2>
-                    </div>
-                    <div className="row wow fadeInUp">
-                        <div className="col-lg-3 col-sm-6">
-                            {/* <!-- Team block Seven --> */}
-                            <div className="team-block">
-                                <div className="inner-box">
-                                    <div className="image-box">
-                                        <figure className="image"><a href="page-team-details.html"><img src="images/resource/team-1.jpg" alt="" /></a></figure>
-                                    </div>
-                                    <div className="info-box">
-                                        <h4 className="name"><a href="page-team-details.html">Kevin Hardson</a></h4>
-                                        <span className="designation">Engineer</span>
-                                        <div className="social-links">
-                                            <a href="#"><i className="fab fa-facebook-f"></i></a>
-                                            <a href="#"><i className="fab fa-twitter"></i></a>
-                                            <a href="#"><i className="fab fa-instagram"></i></a>
-                                            <a href="#"><i className="fab fa-pinterest-p"></i></a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-3 col-sm-6">
-                            {/* <!-- Team block Seven --> */}
-                            <div className="team-block">
-                                <div className="inner-box">
-                                    <div className="image-box">
-                                        <figure className="image"><a href="page-team-details.html"><img src="images/resource/team-2.jpg" alt="" /></a></figure>
-                                    </div>
-                                    <div className="info-box">
-                                        <h4 className="name"><a href="page-team-details.html">Jerome Bell</a></h4>
-                                        <span className="designation">Project Manager</span>
-                                        <div className="social-links">
-                                            <a href="#"><i className="fab fa-facebook-f"></i></a>
-                                            <a href="#"><i className="fab fa-twitter"></i></a>
-                                            <a href="#"><i className="fab fa-instagram"></i></a>
-                                            <a href="#"><i className="fab fa-pinterest-p"></i></a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-3 col-sm-6">
-                            {/* <!-- Team block Seven --> */}
-                            <div className="team-block">
-                                <div className="inner-box">
-                                    <div className="image-box">
-                                        <figure className="image"><a href="page-team-details.html"><img src="images/resource/team-3.jpg" alt="" /></a></figure>
-                                    </div>
-                                    <div className="info-box">
-                                        <h4 className="name"><a href="page-team-details.html">John Brown</a></h4>
-                                        <span className="designation">Engineer</span>
-                                        <div className="social-links">
-                                            <a href="#"><i className="fab fa-facebook-f"></i></a>
-                                            <a href="#"><i className="fab fa-twitter"></i></a>
-                                            <a href="#"><i className="fab fa-instagram"></i></a>
-                                            <a href="#"><i className="fab fa-pinterest-p"></i></a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className="col-lg-3 col-sm-6">
-                            {/* <!-- Team block Seven --> */}
-                            <div className="team-block">
-                                <div className="inner-box">
-                                    <div className="image-box">
-                                        <figure className="image"><a href="page-team-details.html"><img src="images/resource/team-4.jpg" alt="" /></a></figure>
-                                    </div>
-                                    <div className="info-box">
-                                        <h4 className="name"><a href="page-team-details.html">Courtney Henry</a></h4>
-                                        <span className="designation">Engineer</span>
-                                        <div className="social-links">
-                                            <a href="#"><i className="fab fa-facebook-f"></i></a>
-                                            <a href="#"><i className="fab fa-twitter"></i></a>
-                                            <a href="#"><i className="fab fa-instagram"></i></a>
-                                            <a href="#"><i className="fab fa-pinterest-p"></i></a>
-                                        </div>
-                                    </div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </div>
-            </section>
-            {/* <!-- End Team Section --> */}
+  <section className="team-section">
+      <div className="auto-container">
+          <div className="sec-title text-center">
+              <span className="sub-title">MEET PROFESSIONALS</span>
+              <h2>Our Expert Members</h2>
+          </div>
+          <div className="four-items-carousel owl-carousel owl-theme default-navs wow fadeInUp">
+              {/* <!-- Team block Seven --> */}
+              <div className="team-block">
+                  <div className="inner-box">
+                      <div className="image-box">
+                          <figure className="image"><a href="page-team-details.html"><img src="images/resource/team-1.jpg" alt=""/></a></figure>
+                      </div>
+                      <div className="info-box">
+                          <h4 className="name"><a href="page-team-details.html">Kevin Hardson</a></h4>
+                          <span className="designation">Engineer</span>
+                          <div className="social-links">
+                              <a href="#"><i className="fab fa-facebook-f"></i></a>
+                              <a href="#"><i className="fab fa-twitter"></i></a>
+                              <a href="#"><i className="fab fa-instagram"></i></a>
+                              <a href="#"><i className="fab fa-pinterest-p"></i></a>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              {/* <!-- Team block Seven --> */}
+              <div className="team-block">
+                  <div className="inner-box">
+                      <div className="image-box">
+                          <figure className="image"><a href="page-team-details.html"><img src="images/resource/team-2.jpg" alt=""/></a></figure>
+                      </div>
+                      <div className="info-box">
+                          <h4 className="name"><a href="page-team-details.html">Jerome Bell</a></h4>
+                          <span className="designation">Project Manager</span>
+                          <div className="social-links">
+                              <a href="#"><i className="fab fa-facebook-f"></i></a>
+                              <a href="#"><i className="fab fa-twitter"></i></a>
+                              <a href="#"><i className="fab fa-instagram"></i></a>
+                              <a href="#"><i className="fab fa-pinterest-p"></i></a>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              {/* <!-- Team block Seven --> */}
+              <div className="team-block">
+                  <div className="inner-box">
+                      <div className="image-box">
+                          <figure className="image"><a href="page-team-details.html"><img src="images/resource/team-3.jpg" alt=""/></a></figure>
+                      </div>
+                      <div className="info-box">
+                          <h4 className="name"><a href="page-team-details.html">John Brown</a></h4>
+                          <span className="designation">Engineer</span>
+                          <div className="social-links">
+                              <a href="#"><i className="fab fa-facebook-f"></i></a>
+                              <a href="#"><i className="fab fa-twitter"></i></a>
+                              <a href="#"><i className="fab fa-instagram"></i></a>
+                              <a href="#"><i className="fab fa-pinterest-p"></i></a>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              {/* <!-- Team block Seven --> */}
+              <div className="team-block">
+                  <div className="inner-box">
+                      <div className="image-box">
+                          <figure className="image"><a href="page-team-details.html"><img src="images/resource/team-4.jpg" alt=""/></a></figure>
+                      </div>
+                      <div className="info-box">
+                          <h4 className="name"><a href="page-team-details.html">Courtney Henry</a></h4>
+                          <span className="designation">Engineer</span>
+                          <div className="social-links">
+                              <a href="#"><i className="fab fa-facebook-f"></i></a>
+                              <a href="#"><i className="fab fa-twitter"></i></a>
+                              <a href="#"><i className="fab fa-instagram"></i></a>
+                              <a href="#"><i className="fab fa-pinterest-p"></i></a>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              {/* <!-- Team block Seven --> */}
+              <div className="team-block">
+                  <div className="inner-box">
+                      <div className="image-box">
+                          <figure className="image"><a href="page-team-details.html"><img src="images/resource/team-2.jpg" alt=""/></a></figure>
+                      </div>
+                      <div className="info-box">
+                          <h4 className="name"><a href="page-team-details.html">Jerome Bell</a></h4>
+                          <span className="designation">Project Manager</span>
+                          <div className="social-links">
+                              <a href="#"><i className="fab fa-facebook-f"></i></a>
+                              <a href="#"><i className="fab fa-twitter"></i></a>
+                              <a href="#"><i className="fab fa-instagram"></i></a>
+                              <a href="#"><i className="fab fa-pinterest-p"></i></a>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+              {/* <!-- Team block Seven --> */}
+              <div className="team-block">
+                  <div className="inner-box">
+                      <div className="image-box">
+                          <figure className="image"><a href="page-team-details.html"><img src="images/resource/team-3.jpg" alt=""/></a></figure>
+                      </div>
+                      <div className="info-box">
+                          <h4 className="name"><a href="page-team-details.html">Jenny Wilson</a></h4>
+                          <span className="designation">Team Leader</span>
+                          <div className="social-links">
+                              <a href="#"><i className="fab fa-facebook-f"></i></a>
+                              <a href="#"><i className="fab fa-twitter"></i></a>
+                              <a href="#"><i className="fab fa-instagram"></i></a>
+                              <a href="#"><i className="fab fa-pinterest-p"></i></a>
+                          </div>
+                      </div>
+                  </div>
+              </div>
+          </div>
+      </div>
+  </section>
+  {/* <!-- End Team Section --> */}
+
         </>
     )
 }
