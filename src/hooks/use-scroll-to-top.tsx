c26f97d99ba99be43df5c35ee6a8ce30e0748c93
jegatheesprana <jegatheesprana@gmail.com>
2023-11-28 21:51:04 +0530

    scroll animation fixed

--- src/hooks/use-scroll-to-top.tsx
+++ src/hooks/use-scroll-to-top.tsx
@@ -15,6 +15,25 @@ export function useScrollToTop() {
         window.appear()
         // @ts-ignore
         window.fancyBox()
+
+        // @ts-ignore
+        if (window.$(".wow").length) {
+            // @ts-ignore
+            var wow = new window.WOW({
+                boxClass: "wow", // animated element css class (default is wow)
+                animateClass: "animated", // animation css class (default is animated)
+                offset: 0, // distance to the element when triggering the animation (default is 0)
+                mobile: false, // trigger animations on mobile devices (default is true)
+                live: true, // act on asynchronously loaded content (default is true)
+            })
+            wow.init()
+        }
+
+        // @ts-ignore
+        if (window.$(".filter-list").length) {
+            // @ts-ignore
+            window.$(".filter-list").mixItUp({})
+        }
     }, [pathname])
 
     return null
