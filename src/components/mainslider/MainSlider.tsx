e4d7f8d8c81e2bf95034c4d258b62a9a02ea43e8
jegatheesprana <jegatheesprana@gmail.com>
2024-01-17 22:47:29 +0530

    slider

--- src/components/mainslider/MainSlider.tsx
+++ src/components/mainslider/MainSlider.tsx
@@ -93,7 +93,7 @@ export default function MainSlider({}: Props) {
                                 </div>
                             </li>
 
-                            {/* <!-- Slide 1 --> */}
+                            {/* <!-- Slide 2 --> */}
                             <li data-index="rs-2" data-transition="zoomout">
                                 {/* <!-- MAIN IMAGE --> */}
                                 <img src="/images/main-slider/2.jpg" alt="" className="rev-slidebg" />
@@ -140,7 +140,82 @@ export default function MainSlider({}: Props) {
                                 >
                                     <span className="title">
                                         Ecology Limited are a fully independent, specialist ecological consultancy,
-                                        working across the UK
+                                        working across the Sri Lanka
+                                    </span>
+                                </div>
+
+                                <div
+                                    className="tp-caption"
+                                    data-paddingbottom="[0,0,0,0]"
+                                    data-paddingleft="[15,15,15,15]"
+                                    data-paddingright="[15,15,15,15]"
+                                    data-paddingtop="[0,0,0,0]"
+                                    data-responsive_offset="on"
+                                    data-type="text"
+                                    data-height="none"
+                                    data-width="['700','750','700','450']"
+                                    data-whitespace="normal"
+                                    data-hoffset="['0','0','0','0']"
+                                    data-voffset="['120','120','150','120']"
+                                    data-x="['left','left','left','left']"
+                                    data-y="['middle','middle','middle','middle']"
+                                    data-textalign="['top','top','top','top']"
+                                    data-frames='[{"delay":1000,"speed":1500,"frame":"0","from":"y:[-100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;","mask":"x:0px;y:0px;s:inherit;e:inherit;","to":"o:1;","ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"auto:auto;","ease":"Power3.easeInOut"}]'
+                                >
+                                    <RouterLink href="/news" className="theme-btn btn-style-one hvr-light">
+                                        <span className="btn-title">Explore More</span>
+                                    </RouterLink>
+                                </div>
+                            </li>
+
+                            {/* <!-- Slide 3 --> */}
+                            <li data-index="rs-2" data-transition="zoomout">
+                                {/* <!-- MAIN IMAGE --> */}
+                                <img src="/images/main-slider/4.png" alt="" className="rev-slidebg" />
+                                <div
+                                    className="tp-caption"
+                                    data-paddingbottom="[0,0,0,0]"
+                                    data-paddingleft="[15,15,15,15]"
+                                    data-paddingright="[15,15,15,15]"
+                                    data-paddingtop="[0,0,0,0]"
+                                    data-responsive_offset="on"
+                                    data-type="text"
+                                    data-height="none"
+                                    data-width="['750','750','750','650']"
+                                    data-whitespace="normal"
+                                    data-hoffset="['0','0','0','0']"
+                                    data-voffset="['-150','-40','0','-30']"
+                                    data-x="['left','left','left','left']"
+                                    data-y="['middle','middle','middle','middle']"
+                                    data-textalign="['top','top','top','top']"
+                                    data-frames='[{"delay":1000,"speed":1500,"frame":"0","from":"y:[-100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;","mask":"x:0px;y:0px;s:inherit;e:inherit;","to":"o:1;","ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"auto:auto;","ease":"Power3.easeInOut"}]'
+                                >
+                                    <h2>
+                                        Energize Society <br />
+                                        Reliable Energy
+                                    </h2>
+                                </div>
+                                <div
+                                    className="tp-caption"
+                                    data-paddingbottom="[0,0,0,0]"
+                                    data-paddingleft="[15,15,15,15]"
+                                    data-paddingright="[0,0,0,0]"
+                                    data-paddingtop="[0,0,0,0]"
+                                    data-responsive_offset="on"
+                                    data-type="text"
+                                    data-height="none"
+                                    data-width="['650','650','650','470']"
+                                    data-whitespace="normal"
+                                    data-hoffset="['0','0','0','0']"
+                                    data-voffset="['0','-160','-120','-170']"
+                                    data-x="['left','left','left','left']"
+                                    data-y="['middle','middle','middle','middle']"
+                                    data-textalign="['top','top','top','top']"
+                                    data-frames='[{"delay":1000,"speed":1500,"frame":"0","from":"y:[-100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;","mask":"x:0px;y:0px;s:inherit;e:inherit;","to":"o:1;","ease":"Power3.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"auto:auto;","ease":"Power3.easeInOut"}]'
+                                >
+                                    <span className="title">
+                                        Ecology Limited are a fully independent, specialist ecological consultancy,
+                                        working across the Sri Lanka
                                     </span>
                                 </div>
 
